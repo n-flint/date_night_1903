36e0c010311522eeb1e2eb5d233fa6520e1c837b
Noah Flint <noah.flint24@gmail.com>
2019-07-15 09:27:19 -0600

    test it exists for binary search tree

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -1,6 +1,12 @@
 require './test/test_helper'
+require './lib/binary_search_tree'
 
 class BinarySearchTreeTest < Minitest::Test
 
+  def test_it_exists
+    tree = BinarySearchTree.new
+
+    assert_instance_of BinarySearchTree, tree
+  end
 
 end
