448ac0833982879c4f68acc605f8c58d663105d1
Noah Flint <noah.flint24@gmail.com>
2019-07-15 11:00:11 -0600

    required node.rb, test for changing root

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -15,16 +15,24 @@ class BinarySearchTreeTest < Minitest::Test
     assert_equal tree.root, nil
   end
 
+  def test_first_node_is_set_to_root
+    tree = BinarySearchTree.new
+    first_node = tree.insert(61, "Bill & Ted's Excellent Adventure")
+
+    assert_equal tree.root, first_node
+  end
+
   def test_it_can_insert_nodes
+    skip
     tree = BinarySearchTree.new
     first_node_depth = tree.insert(61, "Bill & Ted's Excellent Adventure")
     second_node_depth = tree.insert(16, "Johnny English")
     third_node_depth = tree.insert(92, "Sharknado 3")
     fourth_node_depth = tree.insert(50, "Hannibal Buress: Animal Furnace")
     
-    asser_equal first_node_depth, 0
-    asser_equal second_node_depth, 1
-    asser_equal third_node_depth, 1
-    asser_equal fourth_node_depth, 2
+    assert_equal first_node_depth, 0
+    assert_equal second_node_depth, 1
+    assert_equal third_node_depth, 1
+    assert_equal fourth_node_depth, 2
   end
 end
