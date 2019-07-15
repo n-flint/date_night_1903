c0641432d87eb46eb549078a3a3867fe88b23487
Noah Flint <noah.flint24@gmail.com>
2019-07-15 09:53:28 -0600

    test for inserting nodes

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -9,4 +9,16 @@ class BinarySearchTreeTest < Minitest::Test
     assert_instance_of BinarySearchTree, tree
   end
 
+  def test_it_can_insert_nodes
+    tree = BinarySearchTree.new
+    first_node_depth = tree.insert(61, "Bill & Ted's Excellent Adventure")
+    second_node_depth = tree.insert(16, "Johnny English")
+    third_node_depth = tree.insert(92, "Sharknado 3")
+    fourth_node_depth = tree.insert(50, "Hannibal Buress: Animal Furnace")
+    
+    asser_equal first_node_depth, 0
+    asser_equal second_node_depth, 1
+    asser_equal third_node_depth, 1
+    asser_equal fourth_node_depth, 2
+  end
 end
